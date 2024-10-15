b4ea8bc56f2a30ee013e8716d5250990498db091
Max Joseph <maxjoseph22@gmail.com>
2024-10-15 09:58:11 +0100

    added test

---
+++ tests/test_album.py
@@ -0,0 +1,5 @@
+# When I construct an album with the fields title, release year
+# and artist_id, they are reflected in the instance properties
+
+# def test_constructs_with_fields():
+#     album = Album()
